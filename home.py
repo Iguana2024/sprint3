b0ae30f2723f0f1f707af623a651a0286efec91a
AlanaSahaidak <sahaidak_alana@ukr.net>
2024-05-10 00:12:56 +0300

    Added some modification in functions. Change visual foe home.html

--- home.py
+++ home.py
@@ -1,31 +1,61 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+import pymongo
 from pymongo import MongoClient
 import hashlib
 import datetime
+import uuid
 
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017/')
-db = client['data_storage']
+mongo_client = MongoClient('mongodb://mongodb:27017/')
+db = mongo_client['data_storage']
 permissions = db['permissions']
 rejections = db['rejections']
-counter = db['counter']
 
 
-if counter.count_documents({}) == 0:
-    counter.insert_one({'count': 1})
+def test_mongo_connection():
+    try:
+        info = mongo_client.server_info()
+        return True, info
+    except pymongo.errors.ServerSelectionTimeoutError as err:
+        return False, str(err)
 
-def generate_id():
-    count = counter.find_one()['count']
-    counter.update_one({}, {'$inc': {'count': 1}})
-    return count
+def hash_user_ip(ip):
+    hash_ip = hashlib.sha256(ip.encode()).hexdigest()
+    return hash_ip
+
+def find_permission(ip):
+    return permissions.find_one({'IP': ip})
+
+def find_rejection(ip):
+    hashed_ip = hash_user_ip(ip)
+    return rejections.find_one({'hashed_ip': hashed_ip})
 
 def get_user_decision(ip):
-    if permissions.find_one({'IP': ip}):
+    if find_permission(ip):
         return 'granted'
-    elif rejections.find_one({'hashed_ip': hashlib.sha256(ip.encode()).hexdigest()}):
+    elif find_rejection(ip):
         return 'rejected'
     else:
         return None
+
+def add_permission(ip):
+    permission = {
+        'ID': str(uuid.uuid4()),
+        'IP': ip,
+        'datetime': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'description': 'Granted access'
+    }
+    permissions.insert_one(permission)
+
+def add_rejection(ip):
+    hashed_ip = hash_user_ip(ip)
+    rejection = {
+        'datetime': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'hashed_ip': hashed_ip,
+        'ID': str(uuid.uuid4()),
+        'description': 'Granted reject'
+    }
+    rejections.insert_one(rejection)
 
 @app.route('/')
 def index():
@@ -39,27 +69,25 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
+    existing_permission = permissions.find_one({'IP': request.remote_addr, 'description': 'Granted access'})
+    existing_reject = rejections.find_one({'hashed_ip': hashlib.sha256(request.remote_addr.encode()).hexdigest(), 'description': 'Granted reject'})
+
+    if existing_permission:
+        return redirect(url_for('granted_permission'))
+    elif existing_reject:
+        return redirect(url_for('rejected_permission'))
+
     if request.form.get('grant'):
-        permission = {
-            'ID': generate_id(),
-            'IP': request.remote_addr,
-            'datetime': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            'description': 'Granted access'
-        }
-        permissions.insert_one(permission)
+        add_permission(request.remote_addr)
         return redirect(url_for('granted_permission'))
     elif request.form.get('reject'):
-        hashed_ip = hashlib.sha256(request.remote_addr.encode()).hexdigest()
-        rejection = {
-            'hashed_ip': hashed_ip
-        }
-        rejections.insert_one(rejection)
+        add_rejection(request.remote_addr)
         return redirect(url_for('rejected_permission'))
 
 @app.route('/erase_data')
 def erase_data():
-    permissions.delete_many({})
     rejections.delete_many({})
+    permissions.delete_many({})
     return redirect(url_for('index'))
 
 @app.route('/granted_permission')
@@ -76,5 +104,13 @@ def get_data():
     rejection_data = list(rejections.find({}, {'_id': 0}))
     return jsonify(permission_data=permission_data, rejection_data=rejection_data)
 
+@app.route('/db')
+def db():
+    success, info = test_mongo_connection()
+    if success:
+        return f'MongoDB Server Info: {info}'
+    else:
+        return f'Failed to connect to MongoDB: {info}'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
