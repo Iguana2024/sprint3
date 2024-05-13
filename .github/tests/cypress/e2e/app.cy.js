e0758cab32e4dff10450c80e92ab57cb72018686
Yura Belichenko <64985383+Yura11@users.noreply.github.com>
2024-05-13 23:28:15 +0300

    Update app.cy.js

--- .github/tests/cypress/e2e/app.cy.js
+++ .github/tests/cypress/e2e/app.cy.js
@@ -3,12 +3,7 @@ describe('Demo  Web App Test', () => {
   it('Start page successfully loads', () => {
     cy.visit('/')
   });
-
-  it('Start page shows right content', () => {
-    const rightContent = 'Permission for data processing';
-    cy.visit('/');
-    cy.get('body').should('contain', rightContent);
-  });
+  
 
   it('Application connected to MongoDB', () => {
     const rightContent = 'MongoDB Server Info';
