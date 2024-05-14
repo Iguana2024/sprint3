fca04528c7a237b3cd5571ce15dfcaefddccb048
annaolenych <95705808+annaolenych@users.noreply.github.com>
2024-05-14 12:01:41 +0300

    fix diagram

--- project_diagram.py
+++ project_diagram.py
@@ -4,21 +4,25 @@ from diagrams.onprem.compute import Server
 from diagrams.onprem.database import MongoDB
 from diagrams.programming.language import Python
 from diagrams.aws.network import CloudFront
+from diagrams.custom import Custom
+
 
 with Diagram("Flask Application Architecture", show=False):
 
     user = Users("User")
 
-    flask_app = Server("Flask App")
-    redis_db = MongoDB("Redis")
-    mongo_db = MongoDB("MongoDB")
+    with Cluster("Docker"):
+        flask_app = Server("Flask App")
+        nginx = Custom("Nginx", icon_path="nginx1.png")
+        redis_db = Custom("Redis", icon_path="redis1.png")
+        mongo_db = MongoDB("MongoDB")
 
     cloudfront = CloudFront("HTTPS Traffic")
 
-    user >> cloudfront >> flask_app
+    user >> cloudfront >> nginx >> flask_app 
 
     with Cluster("Flask App Components"):
         flask_app >> redis_db >> mongo_db
-
+        flask_app << redis_db << mongo_db
    
 
